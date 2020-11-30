585259ae4ac27122357291fcdbf484ff0b02f662
softwaree40 <softwaree40@gmail.com>
2020-11-30 01:47:03 -0500

    apply use to get country information

--- index.js
+++ index.js
@@ -71,42 +71,135 @@
 // newSet.push(new Set(arr))
 // console.log(newSet)
 
-const Car = function(make,speed){
+// const Car = function(make,speed){
    
-    this.make = make
-    this.speed = speed
+//     this.make = make
+//     this.speed = speed
 
-}
-let honda = new Car("Honda",30)
-let toyota = new Car("Toyota",70)
+// }
+// let honda = new Car("Honda",30)
+// let toyota = new Car("Toyota",70)
 
-Car.prototype.accelerate = function (){
+// Car.prototype.accelerate = function (){
   
- console.log(`${this.make } Accelerate in a greater speed of  ${this.speed +=10}`)
+//  console.log(`${this.make } Accelerate in a greater speed of  ${this.speed +=10}`)
 
-}
+// }
 
 
-Car.prototype.brake = function (){
-   console.log(`${this.make} Applied a break in ${this.speed -=5}`)
+// Car.prototype.brake = function (){
+//    console.log(`${this.make} Applied a break in ${this.speed -=5}`)
 
-}
-honda.accelerate()
-honda.brake()
-toyota.accelerate()
-toyota.brake()
+// }
+// honda.accelerate()
+// honda.brake()
+// toyota.accelerate()
+// toyota.brake()
 //ES6 class
 //class decleration 
 class Person {
     // The constructor for the class person take arg we as properties we want the object instatiate from the class
-    // This consructor is fire or call when new object is created
+   // This consructor is fire or call when new object is created
   constructor(firstName,birthYear){
       // Once new object is created,constructor is invoke return new object which is store in a variable "john"
      this.firstName = firstName
      this.birthYear = birthYear
 
   }
+  // This method would be added to the prototype properties of the class
+  calcAge (){
 
+      return `${2020 - this.birthYear}`
+
+  }
 
 }
 const john = new Person("John",1980)
+// 4 step take place when new is use on constructor
+//1. new empty object is created {}
+//2. "this" point or reference the new empty object created
+//3. the new object {} is link to the prototype of the class __prototype__
+//4. the constructor return new object created.
+console.log("This is new object Age",john.calcAge())
+// Classes are not hoisted in javaScript.
+//Classes are first-class-citizen
+// classess are executed in strict mode
+
+// getter and setter in js
+
+const account = {
+ owner: "Wale",
+ movements : [200,500,700,10000,5000],
+
+ get latest(){
+    return this.movements.slice(-1).pop()
+  
+ },
+ set latest(mov){
+   this.movements.push(mov)
+
+ }
+
+};
+console.log(account.latest)
+account.latest = 100
+console.log(typeof account.latest)
+// es6 class
+
+class Car{
+
+constructor(make,speed){
+
+  this.make = make,
+  this.speed = speed
+}
+get speedUS(){
+
+    return `${this.make} going at ${this.speed/1.6} mi/h`
+}
+
+set speedUS(speed){
+
+ return `${this.make} going at ${this.speed * 1.6}`
+
+}
+accelerate(){
+
+return`${this.make } Accelerate in a greater speed of  ${this.speed +=10}`
+}
+brake(){
+    return `${this.make} Applied a break in ${this.speed -=5}`
+
+}
+}
+
+const ford = new Car("Ford",120)
+console.log(ford.speedUS)
+// AJAX
+const getCountry = function(country){
+
+  const request = new XMLHttpRequest()
+  request.open("GET",`https://restcountries.eu/rest/v2/name/${country}`)
+  request.send()
+  request.addEventListener("load",function(){
+  
+    const [data] = JSON.parse(this.responseText)
+    console.log(data)
+    const html = `<article>
+    <img class="country_img" src="${data.flag}"/>
+    <div>
+        <h3 class="country_name">${data.name}</h3>
+        <h4 class="country_region">${data.region}</h3>
+        <p class="country_row"><span>Population:</span>${data.population}</p>
+        <p class="country_row"><span>language:</span>${data.languages[0].name}</p>
+        <p class="country_row"><span>Money:</span>${data.currencies[0].name}</p>
+    </div>
+  
+  </article>`
+  let container = document.querySelector("#new-container")
+  container.insertAdjacentHTML("beforeend",html)
+  
+  })
+
+}
+getCountry("usa")
